a477b1b8f90b64cddd90fed1e8f900a1c027474f
darudeSlurpstorm <davis.jack@husky.neu.edu>
2016-05-16 07:33:52 -0400

    why did it stop working

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ var express = require('express');
 var app = express();
 
 
-
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
