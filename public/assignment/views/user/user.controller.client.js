ad823e914cb9e21c374ed89bff3e17ad857a0511
darudeSlurpstorm <davis.jack@husky.neu.edu>
2016-06-19 12:34:41 -0400

    Retouched register validation

--- public/assignment/views/user/user.controller.client.js
+++ public/assignment/views/user/user.controller.client.js
@@ -123,9 +123,9 @@
         function register(uname, pass, vpass, fname, lname) {
 
 
-            var user = {username: uname, password: pass, firstName: fname, lastName: lname};
-
             if ((uname && pass && vpass) && pass === vpass) {
+
+                var user = {username: uname, password: pass, firstName: fname, lastName: lname};
 
                 UserService
                     .register(user)
@@ -136,11 +136,11 @@
                             $location.url("/user/" + myUser._id);
                         },
                         function (error) {
-                            vm.error = "Username already taken.";
+                            vm.error = "Could not register. Username may already be taken.";
                         }
                     );
             } else {
-                vm.error = "Could not register.";
+                vm.error = "Missing info.";
 
                 if (!uname) {
                     vm.usernameErr = "Must have a username.";
@@ -158,18 +158,6 @@
                     vm.vpasswordErr = "Must verify password.";
                 } else {
                     vm.vpasswordErr = null;
-                }
-
-                if (!fname) {
-                    vm.firstNameErr = "Must have a first name.";
-                } else {
-                    vm.firstNameErr = null;
-                }
-
-                if (!lname) {
-                    vm.lastNameErr = "Must have a last name.";
-                } else {
-                    vm.lastNameErr = null;
                 }
 
                 if (!(pass == vpass)) {
