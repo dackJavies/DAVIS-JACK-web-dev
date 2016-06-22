161b74b506a7210e79eb5b26d612d79025e6f98d
darudeSlurpstorm <davis.jack@husky.neu.edu>
2016-06-21 21:00:21 -0400

    Beginning puzzle editor

--- project/app.js
+++ project/app.js
@@ -4,5 +4,4 @@ module.exports = function(app) {
 
     require("./services/user.service.server.js")(app, models);
     require("./services/puzzle.service.server.js")(app, models);
-    // require("./services/friend.service.server.js")(app, models);
 };
--- public/project/views/puzzle/puzzle.controller.client.js
+++ public/project/views/puzzle/puzzle.controller.client.js
@@ -5,15 +5,85 @@
         .controller("PuzzleListController", PuzzleListController)
         .controller("SolvePuzzleController", SolvePuzzleController);
 
-    function NewPuzzleController(PuzzleService) {
+    function NewPuzzleController(PuzzleService, $routeParams, $location) {
 
-        
+        var vm = this;
+
+        vm.assignWords = assignWords;
+        vm.backToAssign = backToAssign;
+
+        function init() {
+
+            vm.userId = $routeParams["uid"];
+
+            vm.choosing = true;
+            vm.building = false;
+
+            vm.word1 = vm.word2 = vm.word3 = vm.word4 = vm.word5 = null;
+
+        }
+
+        init();
+
+        function assignWords() {
+
+            vm.words = [vm.word1, vm.word2, vm.word3, vm.word4, vm.word5];
+
+            vm.choosing = false;
+            vm.building = true;
+
+            $location.url("/user/" + vm.userId + "/puzzle/new");
+
+        }
+
+        function backToAssign() {
+
+            vm.choosing = true;
+            vm.building = false;
+
+        }
 
     }
 
-    function PuzzleListController(PuzzleService) {
+    function PuzzleListController(PuzzleService, $location, $routeParams) {
 
+        var vm = this;
 
+        vm.deletePuzzle = deletePuzzle;
+
+        function init() {
+
+            vm.userId = $routeParams["uid"];
+
+            PuzzleService
+                .findAllPuzzlesForUser(vm.userId)
+                .then(
+                    function(succ) {
+                        vm.puzzles = succ.data;
+                    },
+                    function(err) {
+                        vm.error = "Could not retrieve puzzles.";
+                    }
+                )
+
+        }
+
+        init();
+
+        function deletePuzzle(puzzleId) {
+
+            PuzzleService
+                .deletePuzzle(puzzleId)
+                .then(
+                    function(succ) {
+                        $location.url("/user/" + vm.userId + "/puzzle");
+                    },
+                    function(err) {
+                        vm.error = "Could not delete puzzle.";
+                    }
+                )
+
+        }
 
     }
 
